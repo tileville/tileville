a59aecc1b8a2018634201112f1146716ccd8c682
Satyam Bansal <satyam@passageprotocol.com>
2024-02-04 14:33:43 +0530

    :bento: Add assets

--- frontend/src/types.d.ts
+++ frontend/src/types.d.ts
@@ -3,3 +3,30 @@ export type Tile = {
   col: number;
   tile_type: TileType;
 };
+
+enum TileType {
+  Empty,
+  WindMill,
+  Park,
+  Street,
+  Center,
+  Port,
+}
+
+export type PlayerId = {
+  player_address: string;
+  player_id: number;
+};
+
+export type Score = {
+  player_id: number,
+  game_id: number,
+  score: number
+}
+
+export type RemainingMoves = {
+  player_id: number,
+  game_id: number,
+  moves: number
+}
+
