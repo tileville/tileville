d18467d0c71f74f23da745c30425ef254d53a894
Satyam Bansal <satyam@passageprotocol.com>
2024-02-08 16:11:39 +0530

    :lipstick: Add github logo

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -3,3 +3,4 @@ export const TREASURY_ADDRESS =
   'B62qqhL8xfHBpCUTk1Lco2Sq8HitFsDDNJraQG9qCtWwyvxcPADn4EV';
 export const FAUCET_URL = 'https://faucet.minaprotocol.com/';
 export const GAME_ENTRY_FEE_KEY = 'is_entry_fee_paid';
+export const GITHUB_URL = 'https://github.com/satyambnsal/minapolis';
