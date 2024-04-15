0f199436bcb43896e24b62aba9bc66cba6f706a1
Satyam Bansal <satyam@passageprotocol.com>
2024-04-15 17:51:35 +0530

    :recycle: Remove dist folder from git

--- packages/chain/dist/client.config.js
+++
@@ -1,7 +0,0 @@
-import { ClientAppChain } from "@proto-kit/sdk";
-import runtime from "./runtime";
-const appChain = ClientAppChain.fromRuntime(runtime);
-appChain.configure({
-    Runtime: runtime.config,
-});
-export const client = appChain;
