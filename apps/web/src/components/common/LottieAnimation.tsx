4e519c29f6a26e02445f52725f956f88c4fea617
Satyam Bansal <satyamsgsits1994@gmail.com>
2024-06-19 16:13:32 +0530

    :lipstick: Lottie animation changes

--- apps/web/src/components/common/LottieAnimation.tsx
+++ apps/web/src/components/common/LottieAnimation.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
 import Lottie from "react-lottie";
 
-export const LottieAnimation = ({ animationData }: { animationData: any }) => {
+export const LottieAnimation = ({
+  animationData,
+}: {
+  animationData: any;
+  className?: string;
+}) => {
   const defaultOptions = {
     loop: true,
     autoplay: true,
