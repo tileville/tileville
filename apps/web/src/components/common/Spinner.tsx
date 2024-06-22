70681567b6fe0813a37e0bc0c57b69541fe34c95
Yash Mittal <yashmittalmm@gmail.com>
2024-06-22 14:21:27 +0530

    added spinner component

---
+++ apps/web/src/components/common/Spinner.tsx
@@ -0,0 +1,20 @@
+export const Spinner = () => {
+  return (
+    <div className="">
+      <div className="lds-spinner">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
