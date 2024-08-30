20659a74d80d89657f20b494e3f2a01119c54e3f
Yash Mittal <yashmittalmm@gmail.com>
2024-08-30 20:49:53 +0530

    spinner on NFT card

--- apps/web/src/components/NFTModal.tsx
+++ apps/web/src/components/NFTModal.tsx
@@ -79,6 +79,30 @@ const parseTraits = (traits: Json): Trait[] => {
   return [];
 };
 
+const getRarityPercentage = (count: number, total: number) => {
+  return Math.ceil((count / total) * 100);
+};
+
+const getRarityColor = (percentage: number) => {
+  if (percentage < 5) {
+    return "text-red-900";
+  } else if (percentage < 20) {
+    return "text-[#a5a51b]";
+  } else {
+    return "text-primary";
+  }
+};
+
+const getRarityBackgroundColor = (percentage: number) => {
+  if (percentage < 5) {
+    return "bg-[red]/10";
+  } else if (percentage < 20) {
+    return "bg-[yellow]/10";
+  } else {
+    return "bg-primary/30";
+  }
+};
+
 export const NFTModal = ({
   traits,
   img_url,
@@ -250,30 +274,6 @@ export const NFTModal = ({
     }
   };
 
-  const getRarityPercentage = (count: number, total: number) => {
-    return Math.ceil((count / total) * 100);
-  };
-
-  const getRarityBackgroundColor = (percentage: number) => {
-    if (percentage < 5) {
-      return "bg-[red]/10";
-    } else if (percentage < 20) {
-      return "bg-[yellow]/10";
-    } else {
-      return "bg-primary/30";
-    }
-  };
-
-  const getRarityColor = (percentage: number) => {
-    if (percentage < 5) {
-      return "text-red-900";
-    } else if (percentage < 20) {
-      return "text-[#a5a51b]";
-    } else {
-      return "text-primary";
-    }
-  };
-
   const isSufficientBalance = (price: number) => {
     if (!networkStore.address) {
       return true;
@@ -305,7 +305,9 @@ export const NFTModal = ({
   const date = algoliaHitData
     ? new Date(algoliaHitData?.time).toUTCString()
     : "-";
-  // console.log("mint progress", mintProgress, algoliaHitData);
+
+  console.log("mint progress", mintProgress, error);
+
   return (
     <>
       <Dialog.Root>
@@ -379,6 +381,17 @@ export const NFTModal = ({
                   </div>
                 )}
               </div>
+
+              {mintProgress[nftID]?.step > 0 && !error && (
+                <div className="absolute bottom-4 right-4">
+                  <div className="flex items-center gap-2 font-semibold">
+                    <span>MINTING</span>
+                    <span className="flex h-8 w-8 items-center justify-center rounded-full bg-primary">
+                      <Spinner />
+                    </span>
+                  </div>
+                </div>
+              )}
             </div>
           </div>
         </Dialog.Trigger>
