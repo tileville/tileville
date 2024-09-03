79e272df48a853a87576b156cc61ba7761204597
Yash Mittal <yashmittalmm@gmail.com>
2024-09-03 14:50:46 +0530

    Throwing out of game bug fix

--- apps/web/src/constants/index.ts
+++ apps/web/src/constants/index.ts
@@ -18,7 +18,7 @@ export const TREASURY_ADDRESS =
   "B62qqhL8xfHBpCUTk1Lco2Sq8HitFsDDNJraQG9qCtWwyvxcPADn4EV";
 export const FAUCET_URL = "https://faucet.minaprotocol.com/";
 export const GAME_ENTRY_FEE_KEY = "is_entry_fee_paid";
-export const GITHUB_URL = "https://github.com/satyambnsal/minapolis";
+export const GITHUB_URL = "https://github.com/tileville/tileville";
 export const GAME_TUTORIAL_VIDEO_URL = "https://youtu.be/rUd880VHHT0";
 export const MY_GITHUB_URL = "https://github.com/satyambnsal";
 export const FEEDBACK_FORM_URL = "https://forms.gle/PyPU67YmDvQvZ7HF9";
