fe1d403aa5649fa20c950174e9b8b220a3103cc0
Yash Mittal <yashmittalmm@gmail.com>
2024-11-18 14:29:03 +0530

    ui: Add new spinner

--- apps/web/src/components/common/Spinner.tsx
+++ apps/web/src/components/common/Spinner.tsx
@@ -1,3 +1,12 @@
 export const Spinner = ({ className }: { className?: string }) => {
   return <span className={`${className} loader`}></span>;
 };
+
+export const Spinner2 = () => {
+  return (
+    <div className="relative h-8 w-8">
+      <div className="absolute bottom-0 left-0 right-0 top-0 rounded-full border-4 border-transparent"></div>
+      <div className="absolute bottom-0 left-0 right-0 top-0 animate-spin rounded-full border-4 border-primary border-t-transparent"></div>
+    </div>
+  );
+};
