7fafabb990c9b93a415830fff0fdf7cf517b8974
Satyam Bansal <satyamsgsits1994@gmail.com>
2024-11-30 20:53:36 +0530

    Fix incorrect image issue

--- apps/web/src/app/api/mint-nft/constants.ts
+++ apps/web/src/app/api/mint-nft/constants.ts
@@ -6,7 +6,8 @@ export const FEEMASTER_PUBLIC_KEY =
   process.env.NEXT_PUBLIC_FEEMASTER_PUBLIC_KEY ||
   "B62qqhL8xfHBpCUTk1Lco2Sq8HitFsDDNJraQG9qCtWwyvxcPADn4EV";
 export const MINTER_PRIVATE_KEY = process.env.MINTER_PRIVATE_KEY || "";
-export const NFT_BUCKET_NAME = process.env.NFT_BUCKET_NAME || "builder_nfts";
+export const TILEVILLE_NFT_BUCKET_NAME = "builder_nfts";
+export const MINATY_NFT_BUCKET_NAME = "minaty_nfts";
 export const MINANFT_CONTRACT_ADDRESS =
   process.env.NEXT_PUBLIC_CONTRACT_ADDRESS || "";
 
