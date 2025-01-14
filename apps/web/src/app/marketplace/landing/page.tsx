3ea2bec39dca63a3b6a1213c748e9b90dcd83a00
Satyam Bansal <satyamsgsits1994@gmail.com>
2025-01-14 23:54:09 +0530

    refactor: Refactor code of new marketplace

--- apps/web/src/app/marketplace/landing/page.tsx
+++ apps/web/src/app/marketplace/landing/page.tsx
@@ -1,13 +1,13 @@
 "use client";
 
 import { Spinner2 } from "@/components/common/Spinner";
+import { CreateCollectionContent } from "@/components/Marketplace/CreateCollectionContent";
 import { MarketplaceCarousel } from "@/components/Marketplace/MarketplaceCarousel";
 import { TopNFTCollections } from "@/components/Marketplace/TopNFTCollections";
 import { NFTModal } from "@/components/NFTModal";
-import { NFT_COLLECTIONS, NFTCollection, NFTCollectionType } from "@/constants";
+import { NFT_COLLECTIONS, NFTCollectionType } from "@/constants";
 import { useFeaturedNFTs } from "@/db/react-query-hooks";
 import { useFetchNFTSAlgolia } from "@/hooks/useFetchNFTSAlgolia";
-import Image from "next/image";
 
 type featuredNFTType = {
   collection: NFTCollectionType;
@@ -21,45 +21,12 @@ export default function MarketplaceLanding() {
   });
   return (
     <div className="mx-auto max-w-[1274px] p-4 pb-8 pt-12 md:pt-20">
-      {/* Hero Section with Carousel */}
       <section className="mb-12">
         <MarketplaceCarousel />
       </section>
       <TopNFTCollections />
 
-      <section>
-        <div className="rounded-xl border-primary bg-[#99B579]">
-          <div className="grid grid-cols-2 items-center gap-4">
-            <div>
-              <Image
-                src="/img/avatars/nftsPreview.png"
-                width={600}
-                height={600}
-                alt="nfts preview"
-                className="-ml-16"
-              />
-            </div>
-
-            <div>
-              <h2 className="text-[40px] font-extrabold text-primary">
-                CREATE YOUR OWN COLLECTION
-              </h2>
-
-              <p className="my-6 max-w-[333px] text-xl font-semibold">
-                Create your unique NFT collection on{" "}
-                <b> TILEVILLE MARKETPLACE: </b>
-                unleash your creativity now
-              </p>
-
-              <div>
-                <button className="min-h-[60px] rounded-lg bg-primary px-5 py-2 text-base font-bold text-white hover:bg-primary/80 md:min-w-[250px]">
-                  CREATE
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
+    <CreateCollectionContent />
 
       <section className="mt-8">
         <div className="grid grid-cols-12">
@@ -85,6 +52,7 @@ export default function MarketplaceLanding() {
                         category,
                         is_public_mint,
                       } = nft;
+
                       const isNftAlreadyMinted = mintNFTHitsResponse.find(
                         ({ name: nftName }) => nftName === name
                       );
@@ -99,9 +67,7 @@ export default function MarketplaceLanding() {
                           nftID={nft_id}
                           nftPrice={price}
                           ownerAddress={owner_address}
-                          algoliaHitData={mintNFTHitsResponse.find(
-                            ({ name }) => name === nft.name
-                          )}
+                          algoliaHitData={isNftAlreadyMinted}
                           renderStyle="grid grid-cols-3 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-8 gap-2"
                           collection={collection}
                           NFTCategory={
