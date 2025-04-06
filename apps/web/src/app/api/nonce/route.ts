0002a7c9aaac7f3f5a7b76089a645943013ddd8d
Satyam Bansal <satyamsgsits1994@gmail.com>
2025-04-06 12:24:19 +0530

    fix: Fix nonce issue

--- apps/web/src/app/api/nonce/route.ts
+++ apps/web/src/app/api/nonce/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest } from "next/server";
-import { BLOCKBERRY_API_KEY, BLOCKBERRY_MAINNET_BASE_URL } from "@/constants";
+// import { BLOCKBERRY_API_KEY, BLOCKBERRY_MAINNET_BASE_URL } from "@/constants";
 
 export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url);
@@ -9,26 +9,49 @@ export async function GET(request: NextRequest) {
   }
   try {
     console.log({ wallet_address });
-    const zkAppTxsPromise = getZkAppTxsFromBlockberry(wallet_address);
-    const paymentTxs = getPaymentTxsFromBlockberry(wallet_address);
 
-    const paymentNonce = (await paymentTxs)?.data[0]?.nonce ?? -1;
-    let zkNonce = -1;
-    let found = false;
-    const zkAppTxs = await zkAppTxsPromise;
-    const size = zkAppTxs?.data?.length ?? 0;
-    let i = 0;
-    while (!found && i < size) {
-      if (zkAppTxs?.data[i]?.proverAddress === wallet_address) {
-        zkNonce = zkAppTxs?.data[i]?.nonce;
-        found = true;
+    const nonceResponse = await fetch(
+      "https://minatokens.com/api/v1/info/nonce",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          address: wallet_address,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+          "x-api-key": process.env.MINATOKENS_API_KEY || "",
+        },
       }
-      i++;
+    );
+    const nonceJson = await nonceResponse.json();
+    console.log("Nonce JSON", nonceJson);
+    if (nonceJson?.nonce !== undefined) {
+      return Response.json(
+        { success: true, nonce: nonceJson.nonce },
+        { status: 200 }
+      );
     }
-    const nonce = Math.max(zkNonce, paymentNonce);
-    console.log("nonce", { zkNonce, paymentNonce, nonce });
+    // return Response.json({ success: false, message: "" }, { status: 500 });
+    // const zkAppTxsPromise = getZkAppTxsFromBlockberry(wallet_address);
+    // const paymentTxs = getPaymentTxsFromBlockberry(wallet_address);
 
-    return Response.json({ success: true, nonce: nonce + 1 }, { status: 200 });
+    // const paymentNonce = (await paymentTxs)?.data[0]?.nonce ?? -1;
+    // let zkNonce = -1;
+    // let found = false;
+    // const zkAppTxs = await zkAppTxsPromise;
+    // const size = zkAppTxs?.data?.length ?? 0;
+    // let i = 0;
+    // while (!found && i < size) {
+    //   if (zkAppTxs?.data[i]?.proverAddress === wallet_address) {
+    //     zkNonce = zkAppTxs?.data[i]?.nonce;
+    //     found = true;
+    //   }
+    //   i++;
+    // }
+    // const nonce = Math.max(zkNonce, paymentNonce);
+    // console.log("nonce", { zkNonce, paymentNonce, nonce });
+
+    // return Response.json({ success: true, nonce: nonce + 1 }, { status: 200 });
   } catch (error: any) {
     return Response.json(
       { success: false, message: error.toString() },
@@ -37,47 +60,47 @@ export async function GET(request: NextRequest) {
   }
 }
 
-async function getZkAppTxsFromBlockberry(wallet_address: string) {
-  const options = {
-    method: "GET",
-    headers: {
-      accept: "application/json",
-      "x-api-key": BLOCKBERRY_API_KEY,
-    },
-  };
-  try {
-    const response = await fetch(
-      `${BLOCKBERRY_MAINNET_BASE_URL}/v1/zkapps/accounts/${wallet_address}/txs?size=10&orderBy=DESC&sortBy=AGE`,
-      options
-    );
-    const result = await response.json();
-    //console.log("zkAppTxs", result);
-    return result;
-  } catch (err) {
-    console.error(err);
-    return undefined;
-  }
-}
+// async function getZkAppTxsFromBlockberry(wallet_address: string) {
+//   const options = {
+//     method: "GET",
+//     headers: {
+//       accept: "application/json",
+//       "x-api-key": BLOCKBERRY_API_KEY,
+//     },
+//   };
+//   try {
+//     const response = await fetch(
+//       `${BLOCKBERRY_MAINNET_BASE_URL}/v1/zkapps/accounts/${wallet_address}/txs?size=10&orderBy=DESC&sortBy=AGE`,
+//       options
+//     );
+//     const result = await response.json();
+//     //console.log("zkAppTxs", result);
+//     return result;
+//   } catch (err) {
+//     console.error(err);
+//     return undefined;
+//   }
+// }
 
-async function getPaymentTxsFromBlockberry(wallet_address: string) {
-  const options = {
-    method: "GET",
-    headers: {
-      accept: "application/json",
-      "x-api-key": BLOCKBERRY_API_KEY,
-    },
-  };
+// async function getPaymentTxsFromBlockberry(wallet_address: string) {
+//   const options = {
+//     method: "GET",
+//     headers: {
+//       accept: "application/json",
+//       "x-api-key": BLOCKBERRY_API_KEY,
+//     },
+//   };
 
-  try {
-    const response = await fetch(
-      `${BLOCKBERRY_MAINNET_BASE_URL}/v1/accounts/${wallet_address}/txs?page=0&size=1&orderBy=DESC&sortBy=AGE&direction=OUT`,
-      options
-    );
-    const result = await response.json();
-    //console.log("paymentTxs", result);
-    return result;
-  } catch (err) {
-    console.error(err);
-    return undefined;
-  }
-}
+//   try {
+//     const response = await fetch(
+//       `${BLOCKBERRY_MAINNET_BASE_URL}/v1/accounts/${wallet_address}/txs?page=0&size=1&orderBy=DESC&sortBy=AGE&direction=OUT`,
+//       options
+//     );
+//     const result = await response.json();
+//     //console.log("paymentTxs", result);
+//     return result;
+//   } catch (err) {
+//     console.error(err);
+//     return undefined;
+//   }
+// }
