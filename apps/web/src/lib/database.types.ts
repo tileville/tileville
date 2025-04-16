707ef394409160c806d61a78eab1cf19bc3d5656
Yash Mittal <yashmittalmm@gmail.com>
2025-04-16 19:31:09 +0530

    update supabase

--- apps/web/src/lib/database.types.ts
+++ apps/web/src/lib/database.types.ts
@@ -73,6 +73,8 @@ export type Database = {
           floor_price: number | null
           id: string
           image_url: string | null
+          is_mint_public: boolean
+          max_supply: number
           mint_limit_per_wallet: number | null
           mint_price: number
           mint_start_date: string | null
@@ -80,7 +82,6 @@ export type Database = {
           owner_count: number | null
           royalty_fee: number | null
           symbol: string
-          total_supply: number
           total_volume: number | null
           trait_schema: Json | null
           updated_at: string | null
@@ -95,6 +96,8 @@ export type Database = {
           floor_price?: number | null
           id?: string
           image_url?: string | null
+          is_mint_public?: boolean
+          max_supply: number
           mint_limit_per_wallet?: number | null
           mint_price: number
           mint_start_date?: string | null
@@ -102,7 +105,6 @@ export type Database = {
           owner_count?: number | null
           royalty_fee?: number | null
           symbol: string
-          total_supply?: number
           total_volume?: number | null
           trait_schema?: Json | null
           updated_at?: string | null
@@ -117,6 +119,8 @@ export type Database = {
           floor_price?: number | null
           id?: string
           image_url?: string | null
+          is_mint_public?: boolean
+          max_supply?: number
           mint_limit_per_wallet?: number | null
           mint_price?: number
           mint_start_date?: string | null
@@ -124,7 +128,6 @@ export type Database = {
           owner_count?: number | null
           royalty_fee?: number | null
           symbol?: string
-          total_supply?: number
           total_volume?: number | null
           trait_schema?: Json | null
           updated_at?: string | null
@@ -145,7 +148,7 @@ export type Database = {
           mint_date: string | null
           name: string
           owner_address: string | null
-          token_id: number
+          token_id: number | null
           token_uri: string | null
           traits: Json | null
           txn_hash: string | null
@@ -164,7 +167,7 @@ export type Database = {
           mint_date?: string | null
           name: string
           owner_address?: string | null
-          token_id: number
+          token_id?: number | null
           token_uri?: string | null
           traits?: Json | null
           txn_hash?: string | null
@@ -183,7 +186,7 @@ export type Database = {
           mint_date?: string | null
           name?: string
           owner_address?: string | null
-          token_id?: number
+          token_id?: number | null
           token_uri?: string | null
           traits?: Json | null
           txn_hash?: string | null
@@ -363,6 +366,27 @@ export type Database = {
           price?: number
           traits?: Json | null
           txn_hash?: string | null
+        }
+        Relationships: []
+      }
+      nonce_counters: {
+        Row: {
+          counter: number | null
+          id: number
+          last_updated: string | null
+          wallet_address: string
+        }
+        Insert: {
+          counter?: number | null
+          id?: number
+          last_updated?: string | null
+          wallet_address: string
+        }
+        Update: {
+          counter?: number | null
+          id?: number
+          last_updated?: string | null
+          wallet_address?: string
         }
         Relationships: []
       }
@@ -918,7 +942,10 @@ export type Database = {
       [_ in never]: never
     }
     Functions: {
-      [_ in never]: never
+      increment_nonce_counter: {
+        Args: { wallet_addr: string }
+        Returns: number
+      }
     }
     Enums: {
       nft_category:
@@ -936,27 +963,29 @@ export type Database = {
   }
 }
 
-type PublicSchema = Database[Extract<keyof Database, "public">]
+type DefaultSchema = Database[Extract<keyof Database, "public">]
 
 export type Tables<
-  PublicTableNameOrOptions extends
-    | keyof (PublicSchema["Tables"] & PublicSchema["Views"])
+  DefaultSchemaTableNameOrOptions extends
+    | keyof (DefaultSchema["Tables"] & DefaultSchema["Views"])
     | { schema: keyof Database },
-  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
-    ? keyof (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
-        Database[PublicTableNameOrOptions["schema"]]["Views"])
+  TableName extends DefaultSchemaTableNameOrOptions extends {
+    schema: keyof Database
+  }
+    ? keyof (Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"] &
+        Database[DefaultSchemaTableNameOrOptions["schema"]]["Views"])
     : never = never,
-> = PublicTableNameOrOptions extends { schema: keyof Database }
-  ? (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
-      Database[PublicTableNameOrOptions["schema"]]["Views"])[TableName] extends {
+> = DefaultSchemaTableNameOrOptions extends { schema: keyof Database }
+  ? (Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"] &
+      Database[DefaultSchemaTableNameOrOptions["schema"]]["Views"])[TableName] extends {
       Row: infer R
     }
     ? R
     : never
-  : PublicTableNameOrOptions extends keyof (PublicSchema["Tables"] &
-        PublicSchema["Views"])
-    ? (PublicSchema["Tables"] &
-        PublicSchema["Views"])[PublicTableNameOrOptions] extends {
+  : DefaultSchemaTableNameOrOptions extends keyof (DefaultSchema["Tables"] &
+        DefaultSchema["Views"])
+    ? (DefaultSchema["Tables"] &
+        DefaultSchema["Views"])[DefaultSchemaTableNameOrOptions] extends {
         Row: infer R
       }
       ? R
@@ -964,20 +993,22 @@ export type Tables<
     : never
 
 export type TablesInsert<
-  PublicTableNameOrOptions extends
-    | keyof PublicSchema["Tables"]
+  DefaultSchemaTableNameOrOptions extends
+    | keyof DefaultSchema["Tables"]
     | { schema: keyof Database },
-  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
-    ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
+  TableName extends DefaultSchemaTableNameOrOptions extends {
+    schema: keyof Database
+  }
+    ? keyof Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"]
     : never = never,
-> = PublicTableNameOrOptions extends { schema: keyof Database }
-  ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
+> = DefaultSchemaTableNameOrOptions extends { schema: keyof Database }
+  ? Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"][TableName] extends {
       Insert: infer I
     }
     ? I
     : never
-  : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
-    ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
+  : DefaultSchemaTableNameOrOptions extends keyof DefaultSchema["Tables"]
+    ? DefaultSchema["Tables"][DefaultSchemaTableNameOrOptions] extends {
         Insert: infer I
       }
       ? I
@@ -985,20 +1016,22 @@ export type TablesInsert<
     : never
 
 export type TablesUpdate<
-  PublicTableNameOrOptions extends
-    | keyof PublicSchema["Tables"]
+  DefaultSchemaTableNameOrOptions extends
+    | keyof DefaultSchema["Tables"]
     | { schema: keyof Database },
-  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
-    ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
+  TableName extends DefaultSchemaTableNameOrOptions extends {
+    schema: keyof Database
+  }
+    ? keyof Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"]
     : never = never,
-> = PublicTableNameOrOptions extends { schema: keyof Database }
-  ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
+> = DefaultSchemaTableNameOrOptions extends { schema: keyof Database }
+  ? Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"][TableName] extends {
       Update: infer U
     }
     ? U
     : never
-  : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
-    ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
+  : DefaultSchemaTableNameOrOptions extends keyof DefaultSchema["Tables"]
+    ? DefaultSchema["Tables"][DefaultSchemaTableNameOrOptions] extends {
         Update: infer U
       }
       ? U
@@ -1006,21 +1039,23 @@ export type TablesUpdate<
     : never
 
 export type Enums<
-  PublicEnumNameOrOptions extends
-    | keyof PublicSchema["Enums"]
+  DefaultSchemaEnumNameOrOptions extends
+    | keyof DefaultSchema["Enums"]
     | { schema: keyof Database },
-  EnumName extends PublicEnumNameOrOptions extends { schema: keyof Database }
-    ? keyof Database[PublicEnumNameOrOptions["schema"]]["Enums"]
+  EnumName extends DefaultSchemaEnumNameOrOptions extends {
+    schema: keyof Database
+  }
+    ? keyof Database[DefaultSchemaEnumNameOrOptions["schema"]]["Enums"]
     : never = never,
-> = PublicEnumNameOrOptions extends { schema: keyof Database }
-  ? Database[PublicEnumNameOrOptions["schema"]]["Enums"][EnumName]
-  : PublicEnumNameOrOptions extends keyof PublicSchema["Enums"]
-    ? PublicSchema["Enums"][PublicEnumNameOrOptions]
+> = DefaultSchemaEnumNameOrOptions extends { schema: keyof Database }
+  ? Database[DefaultSchemaEnumNameOrOptions["schema"]]["Enums"][EnumName]
+  : DefaultSchemaEnumNameOrOptions extends keyof DefaultSchema["Enums"]
+    ? DefaultSchema["Enums"][DefaultSchemaEnumNameOrOptions]
     : never
 
 export type CompositeTypes<
   PublicCompositeTypeNameOrOptions extends
-    | keyof PublicSchema["CompositeTypes"]
+    | keyof DefaultSchema["CompositeTypes"]
     | { schema: keyof Database },
   CompositeTypeName extends PublicCompositeTypeNameOrOptions extends {
     schema: keyof Database
@@ -1029,6 +1064,22 @@ export type CompositeTypes<
     : never = never,
 > = PublicCompositeTypeNameOrOptions extends { schema: keyof Database }
   ? Database[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"][CompositeTypeName]
-  : PublicCompositeTypeNameOrOptions extends keyof PublicSchema["CompositeTypes"]
-    ? PublicSchema["CompositeTypes"][PublicCompositeTypeNameOrOptions]
+  : PublicCompositeTypeNameOrOptions extends keyof DefaultSchema["CompositeTypes"]
+    ? DefaultSchema["CompositeTypes"][PublicCompositeTypeNameOrOptions]
     : never
+
+export const Constants = {
+  public: {
+    Enums: {
+      nft_category: [
+        "FOUNDER",
+        "DESIGNER",
+        "SOLDIER",
+        "GUARDIAN",
+        "TOTEM",
+        "YOUR_NEW_CATEGORY",
+        "ZKON",
+      ],
+    },
+  },
+} as const
