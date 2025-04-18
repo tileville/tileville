d8ac4135916af9b117ada63e8dcb5f82cb6bb556
Yash Mittal <yashmittalmm@gmail.com>
2025-04-18 20:10:39 +0530

    fix: Increase algolia

--- apps/web/src/hooks/useFetchNFTSAlgolia.tsx
+++ apps/web/src/hooks/useFetchNFTSAlgolia.tsx
@@ -97,7 +97,7 @@ export const useFetchNFTSAlgolia = (options: FetchOptions) => {
     setIsLoading(true); // Set loading to true when starting the fetch
     searchJobs({
       query: options.queryText || "Tileville Builder",
-      hitsPerPage: 450,
+      hitsPerPage: 650,
       currentPage: 0,
       statuses: ["pending", "applied"],
       ...(options.owner ? { owner: options.owner } : {}),
